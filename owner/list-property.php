42b7bcf9aaaaa953cedf24765789a4dbc68ba345
primesolution1998-cloud <primesolution1998@gmail.com>
2026-09-07 07:09:49 +0530

    Clean owner listing page canonical and navigation

--- owner/list-property.php
+++ owner/list-property.php
@@ -1,4 +1,16 @@
 <?php
 require_once __DIR__ . '/_auth.php';
 owner_require_login();
-readfile(dirname(__DIR__) . '/list-property.html');
+$file = dirname(__DIR__) . '/list-property.html';
+if (!is_file($file)) { http_response_code(404); exit('Listing page unavailable.'); }
+$html = file_get_contents($file);
+$html = strtr($html, [
+  'https://leadsindia.in/list-property.html' => 'https://leadsindia.in/list-property',
+  'href="index.html"' => 'href="/"',
+  'href="properties.html"' => 'href="/properties"',
+  "href='index.html'" => "href='/'",
+  "href='properties.html'" => "href='/properties'"
+]);
+header('Content-Type: text/html; charset=UTF-8');
+header('Cache-Control: no-cache, must-revalidate');
+echo $html;
